16539ec8d4623c89714a893e5d3285771be3a19a
Andy <andy_lam222@hotmail.com>
2020-10-02 10:18:07 -0400

    Making the custom hook. Missing Back... fixing some warnings.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
-export function getAppointmentsForDay({ days, appointments, interviewers }, dayWanted) {
+export function getAppointmentsForDay({ days, appointments}, dayWanted) {
   try {
-    return days.filter(day => day["name"] == dayWanted)[0]["appointments"].map(appointment => appointments[appointment]);
+    return days.filter(day => day["name"] === dayWanted)[0]["appointments"].map(appointment => appointments[appointment]);
   }
   catch {
     return [];
@@ -9,10 +9,10 @@ export function getAppointmentsForDay({ days, appointments, interviewers }, dayW
 
 export function getInterview(...params) {
   try {
-    const [{ days, appointments, interviewers}, { student, interviewer }] = params;
+    const [state, { student, interviewer }] = params;
     return {
       "interviewer":
-        { ...interviewers[interviewer] },
+        { ...state.interviewers[interviewer] },
       "student": student
     };
   }
@@ -21,7 +21,7 @@ export function getInterview(...params) {
 
 export function getInterviewsForDay({ days, appointments, interviewers }, interviewWanted) {
   try {
-    return days.filter(day => day["name"] == interviewWanted)[0]["interviewers"].map(interview => interviewers[interview]);
+    return days.filter(day => day["name"] === interviewWanted)[0]["interviewers"].map(interview => interviewers[interview]);
   }
   catch {
     return [];
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,7 +2,19 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([]);
 
-  return { mode };
-} 
+  function transition(update, replace = false) {
+    if (!replace) {
+      setHistory(history.concat[mode]);
+  }
+  setMode(update)
+  }
+
+  function back() {
+    
+  }
+
+  return { mode, transition, back };
+}
 
